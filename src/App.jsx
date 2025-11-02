d0b15355f91b197146ed038486211aba04669169
ibh2511 <isabelle.haugan@gmail.com>
2025-11-02 14:42:46 +0100

    css: regular bold for MUNCH

--- src/App.jsx
+++ src/App.jsx
@@ -210,8 +210,7 @@ export default function App() {
                   >
                     Kranen
                   </a>{" "}
-                  cocktailbar i 13. etasje på{" "}
-                  <span className="munch-skewed">MUNCH</span>
+                  cocktailbar i 13. etasje på <b>MUNCH</b>
                   ca. 200 meter unna (<i>23 års aldersgrense</i>)
                 </p>
                 <p>
